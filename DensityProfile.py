47e5000492b6bd8312efe4f963613e43dccf6528
Po-Hsun Tseng <zengbs@gmail.com>
2020-11-21 23:07:48 +0800

    update

--- DensityProfile.py
+++ DensityProfile.py
@@ -7,15 +7,15 @@ NEWTON_G = 1.0
  
 def f(y, r, params):
     Psi, Phi = y      # unpack current values of y
-    CsSqr, PhiOrigin, Rho0_g, Rho0_DM, ScaleRadius = params  # unpack parameters
-    derivs = [ - 2*Psi/r + 4*np.pi*NEWTON_G*( GasProfile( Phi, PhiOrigin, Rho0_g, CsSqr ) 
-               + NFWProfile( r, ScaleRadius, Rho0_DM ) ) , Psi ]
+    CsSqr, PhiGasOrigin, Rho0_g, Rho0_DM, ScaleRadius = params  # unpack parameters
+    derivs = [ - 2*Psi/r + 4*np.pi*NEWTON_G*( GasDensityProfile( Phi, PhiGasOrigin, Rho0_g, CsSqr ) 
+               + NFWDensityProfile( r, ScaleRadius, Rho0_DM ) ) , Psi ]
     return derivs
 
 
 
 
-def NFWProfile( r, ScaleRadius, Rho0_DM ):
+def NFWDensityProfile( r, ScaleRadius, Rho0_DM ):
 
     a = r / ScaleRadius   
 
@@ -24,54 +24,65 @@ def NFWProfile( r, ScaleRadius, Rho0_DM ):
     return NFWDensityProfile1D
 
 
-def GasProfile( Phi, PhiOrigin, Rho0_g, CsSqr ):
+def GasDensityProfile( Phi, PhiGasOrigin, Rho0_g, CsSqr ):
 
-    GasProfile = Rho0_g * np.exp( -1.0/CsSqr * ( Phi - PhiOrigin ) )
+    GasDensityProfile = Rho0_g * np.exp( - ( Phi - PhiGasOrigin ) / CsSqr )
 
-    return GasProfile
+    return GasDensityProfile
 
 
-def DensityProfile( Radius ):
+    # Exat NFW potential
+def ExactNFWPotential(Rho0_DM, ScaleRadius, ScaleRadius):
+    Exact = -4*np.pi*NEWTON_G*Rho0_DM*np.power(ScaleRadius,3) * np.log(1+r/ScaleRadius) / r
+    return Exact
 
-    dr = 5e-4
+def GetTotalDensityProfile( Radius ):
+
+    dr = 2e-4
     r = np.arange(1e-4, Radius, dr)
 
 
     # Parameters for DM
-    Rho0_DM     = 1e-3
-    ScaleRadius = 1.0
+    Rho0_DM      = 0.0 # 
+    ScaleRadius  = 1.0 # scale radius
 
-    # Exat NFW potential
-    Exact = -4*np.pi*NEWTON_G*Rho0_DM*np.power(ScaleRadius,3) * np.log(1+r/ScaleRadius) / r
 
-    # The potential value at the center of sphere
-    Phi0        = Exact[0]
-    Psi0        = 0
+    # The total potential value at the center of sphere
+    Phi0         = 1e-6
+    Psi0         = 0
 
     # Parameters for gas
-    CsSqr       = 0.2
-    PhiOrigin   = Exact[0]
-    Rho0_g      = 0
+    CsSqr        = 1e-5 # The square of sound speed
+    PhiGasOrigin = Phi0 # Assuming gas potential and DM potential have the same value at the center of sphere
+    Rho0_g       = 1e-2 # Peak density
 
     # Bundle Parameters
-    params = [ CsSqr, PhiOrigin, Rho0_g, Rho0_DM, ScaleRadius ]
+    params = [ CsSqr, PhiGasOrigin, Rho0_g, Rho0_DM, ScaleRadius ]
     
     # Bundle boundary values
     y0     = [ Psi0, Phi0 ]
 
-    Potential = odeint( f, y0, r, args=(params,) )
+    TotalPotential = odeint( f, y0, r, args=(params,) )
 
     fig, ax = plt.subplots( 1, 1, sharex=False, sharey=False )
     fig.subplots_adjust( hspace=0.1, wspace=0.1 )                                                             
     fig.set_size_inches( 10, 5 )
 
-    #ax.plot(r, Potential[:,0], 'o')
-    ax.plot(r, np.absolute(Potential[:,1]), 'x')
-    #ax.plot(r, NFWProfile( r, ScaleRadius, Rho0_DM ), '^')
 
 
+    # Plot total (gas+DM) potential profile
+    ax.plot(r, np.absolute(TotalPotential[:,1]), 'x')
+
+    # Plot numerical NFW denisty profile
+    ax.plot(r, NFWDensityProfile( r, ScaleRadius, Rho0_DM ), '^')
+
+    # Plot numerical gas density profile
+    ax.plot(r, GasDensityProfile( TotalPotential[:,1], [PhiGasOrigin]*TotalPotential[:,1].shape[0],  Rho0_g, CsSqr ), '*' )
+
+    # Plot exact NFW potential profile
+    ExactNFWPotential = ExactNFWPotential(Rho0_DM, ScaleRadius, ScaleRadius)
     ax.plot(r, np.absolute(Exact), '>')
-    
+     
 
 
     ax.set_xscale('log')
@@ -79,9 +90,9 @@ def DensityProfile( Radius ):
 
     plt.show()
 
-    return DensityProfile
+    return GetTotalDensityProfile
 
 
 Radius = 1e1
 
-DensityProfile(Radius)
+GetTotalDensityProfile(Radius)
