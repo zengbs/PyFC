daac1dacfddb4fa651eb541663de278d455a680d
Po-Hsun Tseng <zengbs@gmail.com>
2020-11-26 15:16:23 +0800

    update

--- plot.py
+++ plot.py
@@ -4,14 +4,7 @@ from scipy.integrate import odeint
 from density_profile import *
 
 
-def Plot( Radius ):
-    # Parameters for DM
-    ScaleRadius  = 1   # scale radius
-    
-    # Parameters for gas
-    CsSqr        = 0.2     # The square of sound speed
-
-
+def Plot( Radius, dr0, dr, Rho0_g, CsSqr, ScaleRadius, Rho0_DM ):
 
     fig, ax = plt.subplots( 1, 2, sharex=False, sharey=False )
     fig.subplots_adjust( hspace=0.1, wspace=0.1 )                                                             
@@ -19,21 +12,18 @@ def Plot( Radius ):
     handles0=[]
     handles1=[]
 
-    dr = 1e-4  # cell spacing
-    r = np.arange(1e-4, Radius, dr)
+    r = np.arange(dr0, Radius, dr)
 
     # Gas-only
-    Rho0_DM = 0
-    Rho0_g  = 1
-    GasOnlyPotential = NumericalTotalPotential( r, Rho0_DM, ScaleRadius, Rho0_g, CsSqr )
-    GasOnlyDensity   = NumericalGasDensity( r, Rho0_DM, ScaleRadius, Rho0_g, CsSqr )
+    Rho0_DM_Temp = 0
+    GasOnlyPotential = NumericalTotalPotential( r, Rho0_DM_Temp, ScaleRadius, Rho0_g, CsSqr )
+    GasOnlyDensity   = NumericalGasDensity( r, Rho0_DM_Temp, ScaleRadius, Rho0_g, CsSqr )
     A,=ax[0].plot( r, GasOnlyDensity  , '*', label='Gas density without DM' )
     B,=ax[1].plot( r, GasOnlyPotential, '^', label='Gas potential without DM' )
 
     # DM-only
-    Rho0_DM = 1
-    Rho0_g  = 0
-    DMOnlyPotential  = NumericalTotalPotential( r, Rho0_DM, ScaleRadius, Rho0_g, CsSqr )
+    Rho0_g_Temp  = 0
+    DMOnlyPotential  = NumericalTotalPotential( r, Rho0_DM, ScaleRadius, Rho0_g_Temp, CsSqr )
     ExactDensity     = ExactNFWDensity( r, ScaleRadius, Rho0_DM )
     ExactPotential   = ExactNFWPotential(r, Rho0_DM, ScaleRadius)
     C,=ax[0].plot( r, ExactDensity   ,  ls='-' ,color='k', zorder=9 , label='Exact DM density without Gas' )
@@ -41,15 +31,11 @@ def Plot( Radius ):
     E,=ax[1].plot( r, DMOnlyPotential,     'o'                      , label='DM potential without Gas solving from the Poisson solver' )
     
     # DM and gas
-    Rho0_DM = 1
-    Rho0_g  = 1
     TotalPotential   = NumericalTotalPotential( r, Rho0_DM, ScaleRadius, Rho0_g, CsSqr )
     GasDensity       = NumericalGasDensity( r, Rho0_DM, ScaleRadius, Rho0_g, CsSqr )
     F,=ax[0].plot( r, GasDensity,   '>' , label='Gas density with DM' )
     G,=ax[1].plot( r, TotalPotential, 'x' , label='Gas+DM potential', zorder=1 )
     
-
-
 
     ax[0].set_xscale('log')
     ax[0].set_yscale('log')
@@ -63,8 +49,25 @@ def Plot( Radius ):
     plt.show()
 
 
+# Plot radius
+Radius       = 1e2
 
-Radius = 1e2
+# Integration step
+dr           = 3e-4
 
-Plot(Radius)
+# Initial radius for integration
+dr0          = 1e-4
 
+# Peak gas density
+Rho0_g       = 1
+
+# Sound speed
+CsSqr        = 0.02
+
+# DM scale radius
+ScaleRadius  = 1
+
+# DM scale density
+Rho0_DM      = 1
+
+Plot( Radius, dr0, dr, Rho0_g, CsSqr, ScaleRadius, Rho0_DM )
