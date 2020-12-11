2c16b01fa65f4daef3ba296ce8bf1c6067271c4c
Po-Hsun Tseng <zengbs@gmail.com>
2020-12-11 12:23:34 +0800

    update

--- regression_test.sh
+++ regression_test.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
+
+mv parameters.py parameters_temp.py
+cp regression_test/parameters_example.py parameters.py
+python dump_file.py
 sha1sum UM_IC       regression_test/UM_IC
 sha1sum ExtPotTable regression_test/ExtPotTable
+rm parameters.py
+mv parameters_temp.py parameters.py
