3585665ed0ec7f14d4a77deb7195bc432a310c88
Po-Hsun Tseng <zengbs@gmail.com>
2021-02-26 22:15:23 +0800

    update

--- potential.py
+++ potential.py
@@ -1,30 +1,30 @@
 import numpy as np
 from scipy.integrate import quad
 import parameters as par
-
 import density_profile
 
-def phi (mSqr, func):
-    phi = quad(func, 0, mSqr)[0]
-    return phi
 
-def Pot_Bulge (R, z):
+def Potential_Spheroidal (R, z, fun, c):
     a0 = 1
+    def phi (mSqr, func):
+        phi = quad(func, 0, mSqr)[0]
+        return phi
+
     def SecondTerm( tau ):
         c0 = np.sqrt( 1 - e**2 )*a0
         mSqr  = R**2 / ( tau + a0**2 )
         mSqr += z**2 / ( tau + c0**2 )
         mSqr *= a0**2
-        integrand  = phi(mSqr, density_profile.Bulge)
+        integrand  = phi(mSqr, fun)
         integrand /= tau + a0**2
         integrand /= np.sqrt(tau + c0**2)
         return integrand
 
-    e = np.sqrt( 1 - par.Bulge_q**2 )
+    e = np.sqrt( 1 - c**2 )
 
     Pot  =  -2.0*np.pi*par.NEWTON_G * np.sqrt( 1 - e**2 ) / e
 
-    Pot1 = phi(np.inf, density_profile.Bulge)*np.arcsin(e)
+    Pot1 = phi(np.inf, fun)*np.arcsin(e)
   
     Pot2 = 0.5*a0*e*quad(SecondTerm, 0, np.inf)[0]
 
@@ -32,32 +32,10 @@ def Pot_Bulge (R, z):
 
     return Pot
 
-def Pot_DarkHalo (R, z):
-    a0 = 1
-    def SecondTerm( tau ):
-        c0 = np.sqrt( 1 - e**2 )*a0
-        mSqr  = R**2 / ( tau + a0**2 )
-        mSqr += z**2 / ( tau + c0**2 )
-        mSqr *= a0**2
-        integrand  = phi(mSqr, density_profile.DarkHalo)
-        integrand /= tau + a0**2
-        integrand /= np.sqrt(tau + c0**2)
-        return integrand
-
-    e = np.sqrt( 1 - par.Halo_q**2 )
-
-    Pot  =  -2.0*np.pi*par.NEWTON_G * np.sqrt( 1 - e**2 ) / e
-
-    Pot1 = phi(np.inf, density_profile.DarkHalo)*np.arcsin(e)
-  
-    Pot2 = 0.5*a0*e*quad(SecondTerm, 0, np.inf)[0]
-
-    Pot *= Pot1 - Pot2
-
-    return Pot
 
 
 
 par.Parameters()
-Pot = Pot_DarkHalo(1, 1)
-print(Pot)
+PotBulge    = Potential_Spheroidal(1, 1, density_profile.Bulge   , par.Bulge_q)
+PotDarkHalo = Potential_Spheroidal(1, 1, density_profile.DarkHalo, par.Halo_q )
+print(PotBulge, PotDarkHalo)
