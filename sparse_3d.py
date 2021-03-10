42ca4311c664c92c8464cf56d040049223b2c4c6
Po-Hsun Tseng <zengbs@gmail.com>
2021-03-10 22:40:28 +0800

    update

--- sparse_3d.py
+++ sparse_3d.py
@@ -1,31 +1,34 @@
 import numpy as np
 
 # domain size but excluding boundary
-J = I = 5
+J = I = K = 5
 
-def ij2n(i,j):
-    return i+j*I
+def n2ijk(n):
+    k = int(( n - ( n%(I*J) ) ) / (I*J))
+    i = int((n - k*I*J)%I)
+    j = int((n - I*J*k - i)%I)
+    return i, j, k
 
-def n2ij(n):
-    i = n%I
-    j = int((n-i)/J)
-    return i, j
+# n = 0,...,I*J-1
+# i = 0,...,I-1
+# j = 0,...,J-1
 
 
+sparse = np.zeros((K*J*I, K*J*I), dtype=np.short)
 
-sparse = np.zeros((J*I, J*I), dtype=np.short)
-
-for m in range(I*J):
+for m in range(I*J*K):
   # fix mask
-  mask_i, mask_j = n2ij(m)
-  for n in range(I*J):
+  mask_i, mask_j, mask_k = n2ijk(m)
+  for n in range(I*J*K):
   # swipe entire domain but excluding boundary
-      i, j = n2ij(n)
-      if abs(i-mask_i)==0 and abs(j-mask_j)==0:
-         sparse[m][n] = -4
-      elif abs(i-mask_i)==0 and abs(j-mask_j)==1:
+      i, j, k = n2ijk(n)
+      if abs(i-mask_i)==0 and abs(j-mask_j)==0 and abs(k-mask_k)==0:
+         sparse[m][n] = -6
+      elif abs(i-mask_i)==1 and abs(j-mask_j)==0 and abs(k-mask_k)==0:
          sparse[m][n] = 1
-      elif abs(i-mask_i)==1 and abs(j-mask_j)==0:
+      elif abs(i-mask_i)==0 and abs(j-mask_j)==1 and abs(k-mask_k)==0:
+         sparse[m][n] = 1
+      elif abs(i-mask_i)==0 and abs(j-mask_j)==0 and abs(k-mask_k)==1:
          sparse[m][n] = 1
       else:
          sparse[m][n] = 0
@@ -33,7 +36,7 @@ for m in range(I*J):
 
 
 # plot 2D sparse matrx of 2D domain
-for m in range(I*J):
-   for n in range(I*J):
+for m in range(I*J*K):
+   for n in range(I*J*K):
         print("%+d" % sparse[m][n], end ='')
    print("")
